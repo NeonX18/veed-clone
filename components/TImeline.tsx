059707653d52767247589971d1bd77066d9a0f80
NeonX18 <ishan18@kgpian.iitkgp.ac.in>
2025-03-28 19:40:39 +0530

    update 2

--- components/TImeline.tsx
+++ components/TImeline.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Button,
   Stack,
@@ -9,7 +9,6 @@ import {
   Group,
   Paper,
   Slider,
-  Title,
   Divider,
 } from "@mantine/core";
 import { MediaItem } from "@/lib/types";
